afbe7470bf6f77994ab9893d2cf46c5e40ef45fe
bacs93 <36246209+bacs93@users.noreply.github.com>
2020-03-13 21:31:13 -0300

    Add files via upload

---
+++ Aula2RMIComum/src/comum/Escola.java
@@ -0,0 +1,13 @@
+package comum;
+
+import java.rmi.RemoteException;
+
+public class Escola extends Pessoa {
+	
+	public Escola() throws RemoteException {
+		super();
+	}
+	
+	public static final long serialversionUID = -1905487820086139957L;
+
+}
---
+++ Aula2RMIComum/src/comum/Professor.java
@@ -0,0 +1,12 @@
+package comum;
+
+import java.rmi.RemoteException;
+
+public class Professor extends Pessoa {
+	
+	public Professor() throws RemoteException {
+		super();
+	}
+	private static final long serialversionUID = -240083571435147362L;
+	
+}
